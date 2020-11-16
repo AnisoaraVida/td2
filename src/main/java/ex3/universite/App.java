cd1ade43a66e861c3c697a8854e288490f7d4473
anisoaravida <Olaf120697>
2020-11-16 17:18:10 +0100

    exo3

--- src/main/java/ex3/universite/App.java
+++ src/main/java/ex3/universite/App.java
@@ -1,8 +1,6 @@
 package ex3.universite;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -37,6 +35,57 @@ public class App {
 
     }
 
+
+    public static final Set<Matiere> toutesLesMatieresAnnee(Annee a){
+        Set<Matiere> rtr = new HashSet<>();
+        for(UE ue : a.ues()){
+            for(Matiere m : ue.ects().keySet()){
+                rtr.add(m);
+
+            }
+        }
+        return rtr;
+
+        /*OU pour la boucle
+         * for(UE ue : a.ues()){
+         * rtr.addAll(ue.ects().keySet());
+         * }
+         *
+         * */
+
+    }
+    /*
+    public static final Set<Double> toutesLesNotesAnnee(Annee a) {
+        Set<Double> rtr = new HashSet<Double>(){};
+        for (Matiere m : a.) {
+            for (Matiere m : ue.ects().keySet()) {
+               rtr.add();
+            }
+        }
+        return rtr;
+    }
+*/
+
+    public static final Predicate<Etudiant> defaillant = e -> {
+        Set<Matiere> toutesLesMatieresDeLetudiant =
+                App.toutesLesMatieresAnnee(e.annee());
+        for(Matiere m : toutesLesMatieresDeLetudiant){
+            if(! e.notes().containsKey(m)){
+                return true;
+            }
+        }
+        return false;
+    };
+
+    public static final Predicate<Etudiant> noteEliminatoire = e -> {
+        for(double note : e.notes().values()){
+            if(note < 6) {
+                return true;
+            }
+        }
+        return false;
+    };
+
     public static void question3_1() {
         Matiere m1 = new Matiere("MAT1");
         Matiere m2 = new Matiere("MAT2");
@@ -56,62 +105,13 @@ public class App {
         a1.etudiants();
         Predicate<Etudiant> tousLesEtudiants = etudiant -> true;
         afficheSi("TOUS LES ETUDIANTS", tousLesEtudiants, a1);
-        afficheSi2("TOUS LES ETUDIANTS", tousLesEtudiants, a1);
-    }
-
-    public static final Set<Matiere> toutesLesMatieresAnnee(Annee a){
-        Set<Matiere> rtr = new HashSet<>();
-        for(UE ue : a.ues()){
-            for(Matiere m : ue.ects().keySet()){
-                rtr.add(m);
-            }
-        }
-        return rtr;
-
-        /*OU pour la boucle
-        * for(UE ue : a.ues()){
-        * rtr.addAll(ue.ects().keySet());
-        * }
-        *
-        * */
-
-    }
-
-    public static final Predicate<Etudiant> defaillant = e -> {
-        Set<Matiere> toutesLesMatieresDeLetudiant =
-                App.toutesLesMatieresAnnee(e.annee());
-        for(Matiere m : toutesLesMatieresDeLetudiant){
-            if(! e.notes().containsKey(m)){
-                return true;
-            }
-        }
-        return false;
-    };
-
-    public static void question3_2() {
-        Matiere m1 = new Matiere("MAT1");
-        Matiere m2 = new Matiere("MAT2");
-        UE ue1 = new UE("UE1", Map.of(m1, 2, m2, 2));
-        Matiere m3 = new Matiere("MAT3");
-        UE ue2 = new UE("UE2", Map.of(m3, 1));
-        Annee a1 = new Annee(Set.of(ue1, ue2));
-        Etudiant e1 = new Etudiant("39001", "Alice", "Merveille", a1); e1.noter(m1, 12.0);
-        e1.noter(m2, 14.0);
-        e1.noter(m3, 10.0);
-        //System.out.println(e1);
-        Etudiant e2 = new Etudiant("39002", "Bob", "Eponge", a1); e2.noter(m1, 14.0);
-        e2.noter(m3, 14.0);
-        Etudiant e3 = new Etudiant("39003", "Charles", "Chaplin", a1); e3.noter(m1, 18.0);
-        e3.noter(m2, 5.0);
-        e3.noter(m3, 14.0);
-        a1.etudiants();
-        //Predicate<Etudiant> aDEF = etudiant -> etudiant.notes().keySet()>10;
+        //afficheSi2("TOUS LES ETUDIANTS", tousLesEtudiants, a1);
         afficheSi("ETDUDIANT DEF", defaillant, a1);
+        afficheSi("ETUDIANTS AVEC NOTE ELIMINATOIRE", noteEliminatoire, a1);
     }
 
     public static void main(String[] args) {
         question3_1();
-        question3_2();
     }
 }
 
